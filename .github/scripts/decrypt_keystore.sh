a3203477cd53b283b2575a95c10298263f937c1d
Bryan Sills <bryan@bryansills.com>
2020-05-10 21:30:45 -0500

    Use GPG to encrypt the keystore

---
+++ .github/scripts/decrypt_keystore.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# --batch to prevent interactive command --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_GPG_PASSPHRASE" \
+--output ../../release/upload.jks ../../release/upload.jks.gpg
