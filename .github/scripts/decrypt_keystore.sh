70327ce4e837d2f81a7739520fb5622a70128e08
Bryan Sills <bryan@bryansills.com>
2020-05-10 21:38:13 -0500

    Use the correct paths

--- .github/scripts/decrypt_keystore.sh
+++ .github/scripts/decrypt_keystore.sh
@@ -2,4 +2,4 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$KEYSTORE_GPG_PASSPHRASE" \
---output ../../release/upload.jks ../../release/upload.jks.gpg
+--output release/upload.jks release/upload.jks.gpg
